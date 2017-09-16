86c1cb45635b683428d7e06b4b819260c71e2c27
Bruno Dias <dias.h.bruno@gmail.com>
2017-09-15 22:17:19 -0300

    use nyc to run tests and coverage.

--- compile.mk
+++ compile.mk
@@ -1,12 +1,18 @@
 BABEL=./node_modules/babel-cli/bin/babel.js
 BROWSERIFY=./node_modules/browserify/bin/cmd.js
-MOCHA=./node_modules/mocha/bin/mocha
+MOCHA=./node_modules/mocha/bin/_mocha
+NYC=./node_modules/.bin/nyc
 
 CFLAGS=--plugins transform-es2015-modules-umd
-TEST_CFLAGS=--compilers js:babel-register --require should
+TEST_CFLAGS= --compilers js:babel-register --require should
+TEST_REPORT=
 
 ifeq ("$(DEV)", "1")
 TEST_CFLAGS+= -w
+endif
+
+ifneq ("$(REPORT)", "")
+TEST_REPORT+= --reporter=$(REPORT)
 endif
 
 # Rules for compiling source
